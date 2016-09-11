1386bcb435c04d90182eb29b9e2480c4b414e209
Eduardo San Martin Morote <posva13@gmail.com>
2016-09-11 11:59:29 +0200

    Add example

---
+++ example/Hello.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h1 class="hello__title">{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Hello World!'
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+.hello {
+  &__title {
+    color: #42b983;
+  }
+}
+</style>
---
+++ example/README.md
@@ -0,0 +1,12 @@
+Simple example
+===
+
+Example bundling an UMD package with one `vue` file and exporting stylus and CSS (both minified and unminified) to different files.
+
+## Building
+
+```
+cd example
+node build.js
+ls dist
+```
