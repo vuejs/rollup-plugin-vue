d96acbf73021be6a0dc7a99d1f98054912a3c872
Rahul Kadyan <hi@znck.me>
2016-10-12 21:20:08 +0530

    Remove all defaults

--- src/options.js
+++ src/options.js
@@ -3,13 +3,5 @@ export default {
         customAttrSurround: [[/@/, new RegExp('')], [/:/, new RegExp('')]],
         collapseWhitespace: true,
         removeComments: true,
-        collapseBooleanAttributes: true,
-        removeAttributeQuotes: true,
-        // this is disabled by default to avoid removing
-        // "type" on <input type="text">
-        removeRedundantAttributes: false,
-        useShortDoctype: true,
-        removeEmptyAttributes: true,
-        removeOptionalTags: true,
     },
 };
