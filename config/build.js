606dc154fab8671d7cd387acd6b99dd7472e24b8
Rahul Kadyan <hi@znck.me>
2017-02-28 10:09:09 +0530

    Fix missing dependencies

--- config/build.js
+++ config/build.js
@@ -18,7 +18,35 @@ rollup.rollup({
           entry: 'src/index.js',
           plugins: [
               babel({ runtimeHelpers: true })
-          ]
+          ],
+          external (id) {
+              if (/babel-runtime\/.*/i.test(id)) {
+                  return true
+              }
+
+              return [
+                        'camelcase',
+                        'coffeescript-compiler',
+                        'de-indent',
+                        'debug',
+                        'fs',
+                        'html-minifier',
+                        'less',
+                        'magic-string',
+                        'merge-options',
+                        'node-sass',
+                        'parse5',
+                        'path',
+                        'postcss',
+                        'postcss-modules',
+                        'posthtml',
+                        'posthtml-attrs-parser',
+                        'pug',
+                        'rollup-pluginutils',
+                        'vue-template-es2015-compiler',
+                        'vue-template-validator',
+                    ].indexOf(id) > -1
+          }
       })
       .then(function (bundle) {
           bundle.write({
