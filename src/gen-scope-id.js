1236c316ee70a103aad719fcc18bbea802bfc98f
ktsn <ktsn55@gmail.com>
2017-05-02 09:19:48 +0900

    Unify scope id among various environment

--- src/gen-scope-id.js
+++ src/gen-scope-id.js
@@ -1,11 +1,15 @@
 // utility for generating a uid for each component file
 // used in scoped CSS rewriting
+import path from 'path'
 import hash from 'hash-sum'
 const cache = Object.create(null)
 
 export default function genScopeID (file) {
-    if (!cache[file]) {
-        cache[file] = 'data-v-' + hash(file)
+    const modified = path.relative(process.cwd(), file)
+
+    if (!cache[modified]) {
+        cache[modified] = 'data-v-' + hash(modified)
     }
-    return cache[file]
+
+    return cache[modified]
 }
--- test/expects/scoped-css.js
+++ test/expects/scoped-css.js
@@ -1,3 +1,3 @@
-var scopedCss = { template: "<div class=\"test\">Foo</div>",_scopeId: 'data-v-6e7113ce'};
+var scopedCss = { template: "<div class=\"test\">Foo</div>",_scopeId: 'data-v-4f57af4d'};
 
 export default scopedCss;
