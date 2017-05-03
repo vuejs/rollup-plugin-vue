0f96d0afceb69c1a7436fce2f77ae5688333dbb8
Rahul Kadyan <hi@znck.me>
2017-05-03 12:38:29 +0530

    Update less.js
    Just to trigger CI build.

--- src/style/less.js
+++ src/style/less.js
@@ -1,6 +1,5 @@
 export default async function (style, options) {
     const less = require('less')
-
     const { css, map } = await less.render(style.code, {
         sourceMap: {
             sourceMapFullFilename: style.id,
