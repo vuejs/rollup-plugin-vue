713c5a6ea918a264516ff500b506c31bb4885701
Eduardo San Martin Morote <posva@users.noreply.github.com>
2017-05-09 10:24:20 +0200

    Preserve case in templates (#82)
    * Add failing test for uppercase copmonent
    
    Currently the compiled result of a template yields a lowercase version
    while it should keep the original case.
    
    * Update test.js
    
    Just to trigger CI build.
    
    * 🐛 Set caseSensitive option in html-minifier
    
    Fix #81

---
+++ test/fixtures/compileTemplateLocalComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Msg></Msg>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    Msg: {
+      render: h => h('div', ['Hello']),
+    },
+  },
+}
+</script>
