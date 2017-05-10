855006bb592970d998bb585025ebab63cf38c430
Alex Aleksejevski <alex_chen63@hotmail.com>
2017-05-10 19:17:26 +0800

    Trim templates before pug compiling (#86) (#89)
    * Trim templates before pug compiling (#86)
    
    * Fix a mistake (#86)
    
    * Add few nested tags
    
    * Update expected output for pug.vue

--- test/expects/pug.js
+++ test/expects/pug.js
@@ -1,3 +1,3 @@
-var pug = { template: "<div class=\"pug__test keep-me\">foo</div>",cssModules: {"test":"pug__test"},};
+var pug = { template: "<div class=\"pug__test keep-me\"><article><p>foo</p></article></div>",cssModules: {"test":"pug__test"},};
 
 export default pug;
