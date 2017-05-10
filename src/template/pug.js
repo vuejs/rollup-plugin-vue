d412b8169603ce2e71f39ce6261429878a6e3f1e
ChenGuangjun <chenguangjun@rfchina.com>
2017-05-10 18:40:17 +0800

    Fix a mistake (#86)

--- src/template/pug.js
+++ src/template/pug.js
@@ -1,6 +1,6 @@
 export default async function (template, extras, options) {
     const pug = require('pug')
-    const trim = typeof template === 'string' ? template.trim : template
+    const trim = typeof template === 'string' ? template.trim() : template
     const compiler = pug.compile(trim, { filename: extras.id, ...options.pug })
 
     return compiler({css: extras.modules || {}})
