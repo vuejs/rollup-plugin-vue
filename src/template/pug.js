2fc916e8b4586e684aef61214c06b94d69ea01f5
Herrington Darkholme <HerringtonDarkholme@users.noreply.github.com>
2017-05-12 12:40:24 +0800

    remove trim

--- src/template/pug.js
+++ src/template/pug.js
@@ -1,7 +1,6 @@
 export default async function (template, extras, options) {
     const pug = require('pug')
-    const trim = typeof template === 'string' ? template.trim() : template
-    const compiler = pug.compile(trim, { filename: extras.id, ...options.pug })
+    const compiler = pug.compile(template, { filename: extras.id, ...options.pug })
 
     return compiler({css: extras.modules || {}})
 }
