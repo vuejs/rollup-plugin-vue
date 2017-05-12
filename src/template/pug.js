ba5448c32b324d095ea7872e0dfd868a22e19958
Herrington Darkholme <HerringtonDarkholme@users.noreply.github.com>
2017-05-12 12:50:56 +0800

    specify pug's doctype to use boolean attributes' terse style

--- src/template/pug.js
+++ src/template/pug.js
@@ -1,6 +1,6 @@
 export default async function (template, extras, options) {
     const pug = require('pug')
-    const compiler = pug.compile(template, { filename: extras.id, ...options.pug })
+    const compiler = pug.compile(template, { filename: extras.id, doctype: 'html', ...options.pug })
 
     return compiler({css: extras.modules || {}})
 }
--- test/expects/pug.js
+++ test/expects/pug.js
@@ -1,3 +1,3 @@
-var pug = { template: "<div class=\"pug__test keep-me\" v-if=\"true\"><article><p>foo</p></article></div><p v-else=\"v-else\">nothing</p>",cssModules: {"test":"pug__test"},};
+var pug = { template: "<div class=\"pug__test keep-me\" v-if=\"true\"><article><p>foo</p></article></div><p v-else>nothing</p>",cssModules: {"test":"pug__test"},};
 
 export default pug;
