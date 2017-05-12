7ebf1abd034f94054b30d67e883d20142b7008e6
Herrington Darkholme <HerringtonDarkholme@users.noreply.github.com>
2017-05-12 11:46:27 +0800

    remove redundant re-deindent

--- src/vueTransform.js
+++ src/vueTransform.js
@@ -53,10 +53,10 @@ async function processTemplate (source, id, content, options, nodes, modules) {
 
     const extras = { modules, id, lang: source.attrs.lang }
     const code = deIndent(source.code)
-    const template = deIndent(
-          await (options.disableCssModuleStaticReplacement !== true
-                ? templateProcessor(code, extras, options)
-                : code)
+    const template = await (
+        options.disableCssModuleStaticReplacement !== true
+            ? templateProcessor(code, extras, options)
+            : code
     )
 
     if (!options.compileTemplate) {
