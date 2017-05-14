cbcf049c58161b3df69d1ef658a7024cdbe017bd
Rahul Kadyan <hi@znck.me>
2017-05-14 10:15:02 +0530

    Allow data option to prepend style in scss, less, stylus

--- src/style/less.js
+++ src/style/less.js
@@ -1,12 +1,15 @@
 export default async function (style, options) {
     const less = require('less')
-    const { css, map } = await less.render(style.code, {
-        sourceMap: {
-            sourceMapFullFilename: style.id,
-            sourceMapFileInline: false
-        },
-        ...options.less
-    })
+    const { css, map } = await less.render(
+          'data' in options.less ? `${options.less.data}\n${style.code}` : style.code,
+        {
+            sourceMap: {
+                sourceMapFullFilename: style.id,
+                sourceMapFileInline: false
+            },
+            ...options.less
+        }
+    )
 
     style.$compiled = {
         code: css.toString(),
--- src/style/stylus.js
+++ src/style/stylus.js
@@ -1,10 +1,10 @@
 export default async function (style, options) {
     const stylus = require('stylus')
-    const stylusObj = stylus(style.code, {...options.stylus})
-        .set('filename', style.id)
-        .set('sourcemap', {
-            'comment': false
-        })
+    const stylusObj = stylus('data' in options.stylus ? `${options.stylus.data}\n${style.code}` : style.code, { ...options.stylus })
+          .set('filename', style.id)
+          .set('sourcemap', {
+              'comment': false
+          })
 
     const code = await stylusObj.render()
     const map = stylusObj.sourcemap
