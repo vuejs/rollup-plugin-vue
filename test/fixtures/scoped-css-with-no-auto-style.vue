e165448b0357d4b93fbef21cb6f2cb28acd20837
Rahul Kadyan <hi@znck.me>
2017-05-14 10:01:46 +0530

    Add failing tests for #91

---
+++ test/fixtures/scoped-css-with-no-auto-style.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="test test2">Foo</div>
+</template>
+
+<script lang="babel">
+export default {}
+</script>
+
+<style lang="css" scoped>
+.test {
+    color: red;
+}
+</style>
+
+<style lang="css" module>
+.test2 {
+    color: red;
+}
+</style>
