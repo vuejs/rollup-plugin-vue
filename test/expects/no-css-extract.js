436c07481ce6a19bdb69e7336f235b4a194f197b
Rahul Kadyan <hi@znck.me>
2017-07-24 11:16:24 +0530

    Fix #101, check for existence of document before injecting css (#114)
    * fix: check for existence of document before injecting css
    
    * test: update generated code in fixture

--- test/expects/no-css-extract.js
+++ test/expects/no-css-extract.js
@@ -1,4 +1,4 @@
-(function(){ if(document){ var head=document.head||document.getElementsByTagName('head')[0], style=document.createElement('style'), css=".baz { color: #008000; } .bar { color: blue } .foo { color: red; } "; style.type='text/css'; if (style.styleSheet){ style.styleSheet.cssText = css; } else { style.appendChild(document.createTextNode(css)); } head.appendChild(style); } })();
+(function(){ if(typeof document !== 'undefined'){ var head=document.head||document.getElementsByTagName('head')[0], style=document.createElement('style'), css=".baz { color: #008000; } .bar { color: blue } .foo { color: red; } "; style.type='text/css'; if (style.styleSheet){ style.styleSheet.cssText = css; } else { style.appendChild(document.createTextNode(css)); } head.appendChild(style); } })();
 
 
 
